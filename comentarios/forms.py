4c38322b92fee1ce2dd5cc1d7e77ab9bbbc1b129
Gustavo <gustavofeliperepositorio@gmail.com>
2022-12-19 16:35:23 -0300

    validação reCAPTCHA nos comentarios

--- comentarios/forms.py
+++ comentarios/forms.py
@@ -1,13 +1,37 @@
 from django.forms import ModelForm
 from .models import Comentario
+import requests
 
 class FormComentario(ModelForm): # formulario de detalhes do post
 
     def clean(self): # fazer validações
-        data = self.cleaned_data
-        nome = data.get('nome_comentario') 
-        email = data.get('email_comentario')
-        comentario = data.get('comentario')
+        raw_data = self.data
+        recaptcha_response = raw_data.get('g-recaptcha-response')
+
+        if not recaptcha_response:
+            self.add_error(
+                'comentario',
+                'Confirme que você não é um robô.'
+            )
+        
+        # https://google.com/recaptcha/api/siteverify
+        # secret
+        # response
+
+        recaptcha_request = requests.post(
+            'https://google.com/recaptcha/api/siteverify',
+            data={
+                'secret': '6LdOt5AjAAAAAEURQl4apQdT0tEUTYsaYKrjTvei',
+                'response': recaptcha_response
+            }
+        )
+        recaptcha_result = recaptcha_request.json()
+        # print(recaptcha_result)
+
+        cleaned_data = self.cleaned_data
+        nome = cleaned_data.get('nome_comentario') 
+        email = cleaned_data.get('email_comentario')
+        comentario = cleaned_data.get('comentario')
 
 
     class Meta:
