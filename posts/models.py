8c6ac988b3690ba34610442121f91e8ca3d794d1
Gustavo <gustavofeliperepositorio@gmail.com>
2022-12-19 08:37:07 -0300

    redimensionando imagens dos posts

--- posts/models.py
+++ posts/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from categorias.models import Categoria
 from django.contrib.auth.models import User
 from django.utils import timezone
+from PIL import Image
+from django.conf import settings
+import os
 
 class Post(models.Model):
     titulo_post = models.CharField(max_length=255, verbose_name='Título')
@@ -15,3 +18,23 @@ class Post(models.Model):
 
     def __str__(self):
         return self.titulo_post
+    
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        self.redimensionar_img(self.imagem_post.name, 800)
+
+
+    @staticmethod
+    def redimensionar_img(nome_img, nova_largura): # executa ao salvar a imagem
+        caminho_img = os.path.join(settings.MEDIA_ROOT, nome_img)
+        img = Image.open(caminho_img) 
+        largura, altura = img.size
+        nova_altura = round((nova_largura * altura) / largura)
+
+        if largura <= nova_largura:
+            img.close()
+            return
+
+        nova_img = img.resize((nova_largura, nova_altura), Image.ANTIALIAS)        
+        nova_img.save(caminho_img, optimize=True, quality=60)
