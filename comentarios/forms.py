ff23ce0153127afa6a5238932ebd50e33d1645cc
Gustavo <gustavofeliperepositorio@gmail.com>
2022-12-19 16:55:27 -0300

    removendo campo usuario do form

--- comentarios/forms.py
+++ comentarios/forms.py
@@ -36,4 +36,4 @@ class FormComentario(ModelForm): # formulario de detalhes do post
 
     class Meta:
         model = Comentario
-        fields = ('nome_comentario', 'email_comentario', 'comentario', 'usuario_comentario',)
+        fields = ('nome_comentario', 'email_comentario', 'comentario', )
